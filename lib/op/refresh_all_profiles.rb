0ef56c2c39673d6ee69c60b49423968725a35461
Alex Scarborough <alex@teak.io>
2021-11-09 14:14:08 -0800

    Implement Op::RefreshAllProfiles.

--- lib/op/refresh_all_profiles.rb
+++ lib/op/refresh_all_profiles.rb
@@ -2,7 +2,43 @@
 
 require 'lifecycle/op_base'
 
+require 'aws-sdk-appconfig'
+
 module Op
   class RefreshAllProfiles < Lifecycle::OpBase
+    reads :profile_defs, :applied_profiles, :client_id
+    writes :profiles_to_apply
+
+    def call
+      client = Aws::AppConfig::Client.new
+      self.profiles_to_apply = []
+
+      profile_defs.each do |(profile_name, definition)|
+        request = {
+          application: definition.application, environment: definition.environment,
+          configuration: definition.profile, client_id: client_id
+        }
+
+        current_version = applied_profiles&.fetch(profile_name, nil)&.version
+        request[:client_configuration_version] = current_version if current_version
+
+        response =
+          begin
+            client.get_configuration(request)
+          rescue StandardError => e
+            error profile_name, e
+            nil
+          end
+
+        next if response.nil?
+
+        loaded_version = response.configuration_version
+        next if current_version && loaded_version == current_version
+
+        profiles_to_apply << LoadedProfile.new(
+          profile_name, loaded_version, response.content.read, response.content_type
+        )
+      end
+    end
   end
 end
---
+++ spec/op/refresh_all_profiles_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require 'op/refresh_all_profiles'
+
+require 'configurator_memory'
+require 'configurator_types'
+
+RSpec.describe Op::RefreshAllProfiles do
+  def perform
+    described_class.call(state)
+  end
+
+  before do
+    allow(Aws::AppConfig::Client).to receive(:new).and_return(client_stub)
+    @result = perform
+  end
+
+  subject(:result) { @result }
+
+  let(:state) do
+    ConfiguratorMemory.new(
+      profile_defs: profile_defs,
+      applied_profiles: applied_profiles,
+      client_id: client_id
+    )
+  end
+
+  let(:profile_defs) do
+    {
+      source0: Profile.new(application: 'test', environment: 'test', profile: 'test'),
+      source1: Profile.new(application: 'foo', environment: 'bar', profile: 'boo'),
+      source2: Profile.new(application: 'other', environment: 'test', profile: 'test')
+    }
+  end
+
+  let(:applied_profiles) do
+    {
+      source0: LoadedProfile.new(:source0, '1', { answer: 42 }.to_json, 'application/json'),
+      source1: LoadedProfile.new(:source1, '1', { answer: 255 }.to_json, 'application/json')
+    }
+  end
+
+  let(:client_stub) do
+    Aws::AppConfig::Client.new(stub_responses: true).tap do |client|
+      client.stub_responses(:get_configuration, proc do |request|
+        case request.params[:application]
+        when 'test'
+          { content: StringIO.new({ answer: 42 }.to_json), configuration_version: '1', content_type: 'application/json' }
+        when 'foo'
+          { content: StringIO.new({ answer: 181 }.to_json), configuration_version: '2', content_type: 'application/json' }
+        when 'other'
+          { content: StringIO.new({ answer: 255 }.to_json), configuration_version: '1', content_type: 'application/json' }
+        end
+      end
+      )
+    end
+  end
+
+  let(:client_id) { SecureRandom.uuid }
+
+  context 'when all profiles refresh' do
+    it 'includes versions for applied profiles' do
+      expect(client_stub.api_requests).to contain_exactly(
+        a_hash_including(
+          params: { application: 'test', environment: 'test', configuration: 'test', client_configuration_version: '1',
+                    client_id: client_id }
+        ),
+        a_hash_including(
+          params: { application: 'foo', environment: 'bar', configuration: 'boo', client_configuration_version: '1',
+                    client_id: client_id }
+        ),
+        a_hash_including(
+          params: { application: 'other', environment: 'test', configuration: 'test', client_id: client_id }
+        )
+      )
+    end
+
+    it 'sets the profiles to apply' do
+      expect(state.profiles_to_apply).to contain_exactly(
+        LoadedProfile.new(:source1, '2', { answer: 181 }.to_json, 'application/json'),
+        LoadedProfile.new(:source2, '1', { answer: 255 }.to_json, 'application/json')
+      )
+    end
+  end
+
+  context 'with no applied profiles' do
+    let(:applied_profiles) { nil }
+
+    it 'sets the profiles to apply' do
+      expect(state.profiles_to_apply).to contain_exactly(
+        LoadedProfile.new(:source0, '1', { answer: 42 }.to_json, 'application/json'),
+        LoadedProfile.new(:source1, '2', { answer: 181 }.to_json, 'application/json'),
+        LoadedProfile.new(:source2, '1', { answer: 255 }.to_json, 'application/json')
+      )
+    end
+  end
+
+  context 'when a profile update errors' do
+    let(:client_stub) do
+      Aws::AppConfig::Client.new(stub_responses: true).tap do |client|
+        client.stub_responses(:get_configuration, proc do |request|
+          case request.params[:application]
+          when 'test'
+            { content: StringIO.new, configuration_version: '1', content_type: 'application/json' }
+          when 'foo'
+            { content: StringIO.new({ answer: 181 }.to_json), configuration_version: '2', content_type: 'application/json' }
+          when 'other'
+            'BadRequestException'
+          end
+        end
+        )
+      end
+    end
+
+    it 'errors' do
+      expect(result.errors).to match(
+        source2: [an_instance_of(Aws::AppConfig::Errors::BadRequestException)]
+      )
+    end
+  end
+end
