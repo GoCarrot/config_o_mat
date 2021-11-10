a5d539684cad51a4e5f76175f2f0cacf99eeff62
Alex Scarborough <alex@teak.io>
2021-11-10 11:06:57 -0800

    Implement Op::WaitRetry.

--- lib/op/wait_retry.rb
+++ lib/op/wait_retry.rb
@@ -4,8 +4,22 @@ require 'lifecycle/op_base'
 
 module Op
   class WaitRetry < Lifecycle::OpBase
+    reads :retry_wait, :retry_count, :retries_left
+    writes :retries_left
+
     def call
-      error :base, "not implemented"
+      # Exponential backoff
+      wait = retry_wait * (2**(retry_count - retries_left))
+
+      # With jitter
+      wait += rand(retry_wait + 1) - (retry_wait / 2.0)
+
+      logger&.notice(:retry_wait, wait: wait)
+
+      # Calling Kernel.sleep directly for easy test stubbing
+      Kernel.sleep wait
+
+      self.retries_left -= 1
     end
   end
 end
