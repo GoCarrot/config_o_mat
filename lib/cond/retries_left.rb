687a9d1e53da33c6cc4999a740e33169e649c4ed
Alex Scarborough <alex@teak.io>
2021-11-10 10:50:28 -0800

    Implement Cond::RetriesLeft.

--- lib/cond/retries_left.rb
+++ lib/cond/retries_left.rb
@@ -4,7 +4,17 @@ require 'lifecycle/cond_base'
 
 module Cond
   class RetriesLeft < Lifecycle::CondBase
+    reads :error_op, :retries_left, :applying_profile
+
     def call
+      logger&.error(:op_failure, op: error_op.class.name, errors: error_op.errors)
+
+      # If we aren't currently applying a profile then there's nothing for us to retry.
+      return false if applying_profile.nil?
+
+      # If we're out of retries, well, no more retrying
+      return false if retries_left.zero?
+
       true
     end
   end
---
+++ spec/cond/retries_left_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require 'cond/retries_left'
+
+require 'configurator_memory'
+require 'configurator_types'
+
+RSpec.describe Cond::RetriesLeft do
+  def perform
+    described_class.call(state)
+  end
+
+  before { @result = perform }
+
+  subject(:result) { @result }
+
+  let(:state) do
+    ConfiguratorMemory.new(
+      retries_left: retries_left,
+      applying_profile: applying_profile,
+      logger: logger
+    ).tap do |mem|
+      mem.error_op = error_op
+    end
+  end
+
+  let(:logger) { nil }
+  let(:retries_left) { 3 }
+  let(:applying_profile) { LoadedProfile.new(:source0, '1', { answer: 42 }.to_json, 'application/json') }
+  let(:errors) { { source0: ['syntax error'] } }
+  let(:error_op) do
+    instance_double("Op::StageOneProfile").tap do |dbl|
+      allow(dbl).to receive(:errors?).and_return(true)
+      allow(dbl).to receive(:errors).and_return(errors)
+    end
+  end
+
+  context 'when it can retry' do
+    it 'proceeds with the retry' do
+      expect(result).to be true
+    end
+  end
+
+  context 'when retries_left is 0' do
+    let(:retries_left) { 0 }
+
+    it 'cannot retry' do
+      expect(result).to be false
+    end
+  end
+
+  context 'when no profile is being applied' do
+    let(:applying_profile) { nil }
+
+    it 'cannot retry' do
+      expect(result).to be false
+    end
+  end
+
+  context 'with a logger' do
+    let(:logger) do
+      @messages = []
+      l = LogsForMyFamily::Logger.new
+      l.backends = [proc { |level_name, event_type, merged_data| @messages << [level_name, event_type, merged_data] }]
+      l
+    end
+
+    it 'logs an error' do
+      expect(@messages).to include(
+        contain_exactly(
+          :error, :op_failure, a_hash_including(errors: errors)
+        )
+      )
+    end
+  end
+end
